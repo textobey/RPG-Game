1aff9a193cf34042f2e0909b66b5e5478ae385e6
textObey <dogsan_e@naver.com>
2021-07-31 22:46:28 +0900

    Added interaction of controller

--- RPG/RPG/GameScene/Control.swift
+++ RPG/RPG/GameScene/Control.swift
@@ -9,60 +9,84 @@ import SpriteKit
 import GameplayKit
 
 extension GameScene {
+    // MARK: - Control Ball Angle
+    func controlBallAngle(degree: CGFloat) {
+        // 4방향 지원과 8방향을 지원하는 게임은 준비해야하는 분량이 차이가 크다
+        if degree >= -45 && degree < 45 {
+            print("player's current direction is EAST")
+        } else if degree >= 45 && degree < 135 {
+            print("player's current direction is NORTH")
+        } else if degree >= 135 && degree < 180 {
+            print("player's current direction is WEST")
+        } else if degree >= -180 && degree < -135 {
+            print("player's current direction is WEST")
+        } else if degree >= -135 && degree < -45 {
+            print("player's current direction is SOUTH")
+        }
+    }
+    
+    // Control Ball Angle_End
+    
     // MARK: - Touch
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in (touches) {
             /// 터치된 위치
             let location = touch.location(in: self)
             
-            controlBase.position = location
-            controlBall.position = controlBase.position
-            
-            //touchPoint = location
-            
-            //해당포지션으로 이동
-            //player.position = CGPoint(x: location.x, y: location.y)
-            
-            /// 걷는 모션을 위한 액션
-            //let walkAction = SKAction.move(to: location, duration: 5)
-            
-            // 플레이어는 walkAction 실행
-            //player.run(walkAction)
+            if location.x < 0 {
+                controlBase.position = location
+                controlBall.position = controlBase.position
+            } else {
+                if attackButtonBase.frame.contains(location) {
+                    print("Attack!")
+                } else if itemButton.frame.contains(location) {
+                    print("Item!")
+                } else if skillButton.frame.contains(location) {
+                    print("Skill!")
+                }
+            }
         }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in (touches) {
             let location = touch.location(in: self)
-            /// 삼각형을 구하는 산수
-            let deltaX = location.x - controlBase.position.x
-            let deltaY = location.y - controlBall.position.y
-            /// 밑변/윗변
-            let angle = atan2(deltaY, deltaX)
             
-            let length = controlBase.frame.size.height / 2
-            let distanceX = cos(angle) * length
-            let distanceY = sin(angle) * length
-            
-            if controlBase.frame.contains(location) {
-                // 컨트롤볼안에서의 터치가 이루어지면 컨트롤볼이 움직이게 됨
-                controlBall.position = location
-            } else {
-                // 컨트롤볼이 컨트롤 베이스를 넘어서지 못하게 함
-                controlBall.position = CGPoint(x: controlBase.position.x + distanceX, y: controlBase.position.y + distanceY)
+            if location.x < 0 {
+                /// 삼각형을 구하는 산수
+                let deltaX = location.x - controlBase.position.x
+                let deltaY = location.y - controlBall.position.y
+                /// 밑변/윗변
+                let angle = atan2(deltaY, deltaX)
+                let degree = angle * CGFloat(180 / Double.pi)
+                controlBallAngle(degree: degree)
+                
+                let length = controlBase.frame.size.height / 2
+                let distanceX = cos(angle) * length
+                let distanceY = sin(angle) * length
+                
+                if controlBase.frame.contains(location) {
+                    // 컨트롤볼안에서의 터치가 이루어지면 컨트롤볼이 움직이게 됨
+                    controlBall.position = location
+                } else {
+                    // 컨트롤볼이 컨트롤 베이스를 넘어서지 못하게 함
+                    controlBall.position = CGPoint(x: controlBase.position.x + distanceX, y: controlBase.position.y + distanceY)
+                }
             }
         }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for _ in (touches) {
-            //let location = touch.location(in: self)
+        for touch in (touches) {
+            let location = touch.location(in: self)
             
-            let moveCenterAction = SKAction.move(to: controlBase.position, duration: 0.2)
-            // 컨트롤볼에 대한 터치가 끝났을 때, 0.2초동안 easeOut의 애니메이션으로
-            moveCenterAction.timingMode = .easeOut
-            // 컨트롤볼이 제자리로 돌아오게 됨
-            controlBall.run(moveCenterAction)
+            if location.x < 0 {
+                let moveCenterAction = SKAction.move(to: controlBase.position, duration: 0.2)
+                // 컨트롤볼에 대한 터치가 끝났을 때, 0.2초동안 easeOut의 애니메이션으로
+                moveCenterAction.timingMode = .easeOut
+                // 컨트롤볼이 제자리로 돌아오게 됨
+                controlBall.run(moveCenterAction)
+            }
         }
     }
     // Touch_ End
--- RPG/RPG/GameScene/GameScene.swift
+++ RPG/RPG/GameScene/GameScene.swift
@@ -21,10 +21,25 @@ class GameScene: SKScene {
     let controlBase = SKSpriteNode(imageNamed: "ControlBase")
     /// 컨트롤볼
     let controlBall = SKSpriteNode(imageNamed: "ControlBall")
+    /// 공격버튼
+    let attackButtonBase = SKSpriteNode(imageNamed: "ButtonBase")
+    let attackButton = SKSpriteNode(imageNamed: "AttackButton")
+    /// 아이템버튼
+    let itemButtonBase = SKSpriteNode(imageNamed: "ButtonBase")
+    let itemButton = SKSpriteNode(imageNamed: "ItemButton")
+    /// 스킬버튼
+    let skillButtonBase = SKSpriteNode(imageNamed: "ButtonBase")
+    let skillButton = SKSpriteNode(imageNamed: "SkillButton")
+    
         
     override func didMove(to view: SKView) {
         
         // Controller //
+        
+        // 컨트롤관련 UI보다 위에 위치 하지 않도록함
+        player.zPosition = 10
+        player.position = controlBase.position
+        addChild(player)
         
         // 컨트롤 베이스의 위치 지정과 scene에서의 우선 순위를 지정함, 추가로 player와 겹쳐 표시될때를 고려하여 alpha값 부여
         controlBase.position = CGPoint(x: -500, y: -200)
@@ -32,14 +47,38 @@ class GameScene: SKScene {
         controlBase.alpha = 0.5
         self.addChild(controlBase)
         
+        // 컨트롤볼
         controlBall.position = controlBase.position
         controlBall.zPosition = controlBase.zPosition
         controlBall.alpha = controlBase.alpha
         self.addChild(controlBall)
         
-        // 컨트롤관련 UI보다 위에 위치 하지 않도록함
-        player.zPosition = 10
-        player.position = controlBase.position
-        addChild(player)
+        attackButtonBase.position = CGPoint(x: 375, y: -125)
+        attackButtonBase.zPosition = 99
+        attackButtonBase.alpha = 0.5
+        self.addChild(attackButtonBase)
+        attackButton.position = CGPoint(x: 375, y: -125)
+        attackButton.zPosition = 100
+        attackButton.alpha = 0.5
+        self.addChild(attackButton)
+        
+        itemButtonBase.position = CGPoint(x: 350, y: -300)
+        itemButtonBase.zPosition = 99
+        itemButtonBase.alpha = 0.5
+        self.addChild(itemButtonBase)
+        itemButton.position = CGPoint(x: 350, y: -300)
+        itemButton.zPosition = 100
+        itemButton.alpha = 0.5
+        self.addChild(itemButton)
+        
+        skillButtonBase.position = CGPoint(x: 550, y: -200)
+        skillButtonBase.zPosition = 99
+        skillButtonBase.alpha = 0.5
+        self.addChild(skillButtonBase)
+        skillButton.position = CGPoint(x: 550, y: -200)
+        skillButton.zPosition = 100
+        skillButton.alpha = 0.5
+        
+        self.addChild(skillButton)
     }
 }
