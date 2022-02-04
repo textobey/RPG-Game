fa0dc90772774a1b31d6422b24a107c0a7703452
textobey <dogsan_e@naver.com>
2022-02-04 19:39:43 +0900

    작업환경 설정

---
+++ RPG/RPG/GameScene/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  RPG
+//
+//  Created by 이서준 on 2022/02/04.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    /// 보여지는 화면이 나타나면, 수행되는 메서드
+    override func didMove(to view: SKView) {
+
+    }
+}
